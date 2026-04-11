6fce7487ed862573679dfbe7c83d064a6b0cee2f
GitModu <batatas@gmail.com>
2026-04-11 15:33:04 +0100

    Added Inertia Angle model definition.

---
+++ src/main/java/com/dogecoding/android_embedded/inertia/model/Angle.kt
@@ -0,0 +1,21 @@
+package com.dogecoding.android_embedded.inertia.model
+
+/*
+ * Angle type definition (angle_t = uint16_t).
+ * Maps 360º to an modular integer 16 bit unsigned value.
+ */
+class Angle {
+    companion object {
+        const val PRECISION: Float = 360f / 65536f
+        const val MAX_VALUE: Int = 65535
+        const val MIN_VALUE: Int = 0
+
+        fun degreesFromAngle(angle: Int): Float {
+            return angle * PRECISION
+        }
+
+        fun angleFromDegrees(degrees: Float): Int {
+            return (degrees / PRECISION).toInt()
+        }
+    }
+}
